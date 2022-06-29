40b05a8e64304e497cc828bd90ca7786d3da5b05
Mehmet Emin Ergenç <mergencdev@gmail.com>
2022-06-29 14:09:34 +0300

    -Add GET request endpoint for cast

--- app/src/main/java/com/mergenc/imovs/utils/api/TMDbApiInterface.kt
+++ app/src/main/java/com/mergenc/imovs/utils/api/TMDbApiInterface.kt
@@ -1,8 +1,10 @@
 package com.mergenc.imovs.utils.api
 
+import com.mergenc.imovs.model.Cast.CastResponse
 import com.mergenc.imovs.model.MovieDetails
 import com.mergenc.imovs.model.PopularMoviesResponse
 import io.reactivex.Single
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -13,4 +15,7 @@ interface TMDbApiInterface {
 
     @GET("movie/{movie_id}")
     fun getMovieDetails(@Path("movie_id") id: Int): Single<MovieDetails>
+
+    @GET("movie/{movie_id}/credits")
+    fun getCast(@Path("movie_id") id: Int): Call<CastResponse>
 }
